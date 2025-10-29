34cde894f0c5b9f56fd830f286a0a326c1ccf84c
Asq92 <slanceluna92@gmail.com>
2025-10-29 20:16:58 +0200

    initial commit

---
+++ QA Automation Exercise/01.ExamPreparation-1/01/Program.cs
@@ -0,0 +1,20 @@
+﻿int number = int.Parse(Console.ReadLine());
+
+int sum = 0;
+
+while (number > 0)
+{
+    int lastDigit = number % 10;
+    if (lastDigit % 2 == 0)
+    {
+        int fact = 1;
+        for (int i = 1; i<= lastDigit; i++)
+        {
+            fact *= i;
+        }
+        sum += fact;
+    }
+    number /= 10;
+}
+
+Console.WriteLine(sum);
---
+++ QA Automation Exercise/01.ExamPreparation-1/02/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int firstMiddleElement = numbers[numbers.Length / 2 - 1];
+int secondMiddleElement = numbers[numbers.Length / 2];
+
+double average = (firstMiddleElement + secondMiddleElement) / 2.0;
+
+Console.WriteLine($"{average:F2}");
